3c5c556346ddf1a831b49ad82bdafab36ff09258
magistermgp <70726049+magistermgp@users.noreply.github.com>
2020-11-02 19:38:33 -0500

    What an effing mess this starting code has become

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const readline = require('readline');
-const readlineInterface = readline.createInterface(process.stdin, process.stdout);
+const readline = require("readline");
+const readlineInterface = readline.createInterface(
+  process.stdin,
+  process.stdout
+);
 
 function ask(questionText) {
   return new Promise((resolve, reject) => {
@@ -13,8 +16,17 @@ async function start() {
   const welcomeMessage = `182 Main St.
 You are standing on Main Street between Church and South Winooski.
 There is a door here. A keypad sits on the handle.
-On the door is a handwritten sign.`;
+On the door is a handwritten sign. >_`;
   let answer = await ask(welcomeMessage);
-  console.log('Now write your code to make this work!');
+  while (answer !== "exit") {
+    answer = await ask(">_ ");
+  }
+  if (answer !== "read sign") {
+    console.log("Sorry, I don't know how to " + answer);
+  } else {
+    console.log(
+      "The sign says Welcome to Burlington Code Academy!\n Come on up to the third floor.\n If the door is locked, use the code 12345."
+    );
+  }
   process.exit();
 }
