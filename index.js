ca14bd1fab5d4f1c33208efb537e1f12cbb63a43
magistermgp <70726049+magistermgp@users.noreply.github.com>
2020-11-04 18:25:05 -0500

    trouble piecing it together, less broken but still broken

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { read } = require("fs");
-const readline = require("readline");
+const { read } = require('fs');
+const readline = require('readline');
 const readlineInterface = readline.createInterface(
   process.stdin,
   process.stdout
@@ -20,7 +20,13 @@ class Kitchen {
 
 class Foyer {
   constructor() {
-    this.welcomeMessage = `Welcome to the Foyer`;
+    this.welcomeMessage = `You are in a foyer. Or maybe it's an antechamber. Or a
+vestibule. Or an entryway. Or an atrium. Or a narthex.
+But let's forget all that fancy flatlander vocabulary,
+and just call it a foyer. In Vermont, this is pronounced
+"FO-ee-yurr".
+A copy of Seven Days lies in a corner.`;
+    this.inventory = [new item('sevenDays', this.SevenDays)];
     this.nextRoom = new Kitchen();
   }
 }
@@ -34,17 +40,28 @@ On the door is a handwritten sign. >_`;
     this.sign = `The sign says "Welcome to Burlington Code Academy!
      Come on up to the third floor. 
      If the door is locked, use the code 12345."`;
-    this.inventory = [new item("sign", this.sign), new item("key", 12345)];
+    this.inventory = [new item('sign', this.sign), new item('key', 12345)];
     this.nextRoom = new Foyer();
   }
 
+  openDoor(locked) {
+    console.log('\n');
+    return `The door is locked. There is a keypad on the door handle.`;
+  }
+
+  openDoor(open) {
+    console.log('\n');
+    return `Success! The door opens. 
+    You enter the foyer and the door shuts behind you.`;
+  }
+
   read(thingToRead) {
-    console.log("\n");
+    console.log('\n');
     return this[thingToRead];
   }
 
   take() {
-    console.log("\n");
+    console.log('\n');
     return `That would be selfish. How will other students find their way?`;
   }
 }
@@ -52,13 +69,13 @@ On the door is a handwritten sign. >_`;
 start();
 
 async function start() {
-  console.log("\n");
+  console.log('\n');
   let room = new MainStreet();
   let answer = await ask(room.welcomeMessage);
   // answer === "read sign" => ['read', 'sign'];
-  while (answer !== "exit") {
+  while (answer !== 'exit') {
     //console.log(`Sorry, I don't know how to ${answer}.`);
-    let commands = answer.split(" ");
+    let commands = answer.split(' ');
     let action = commands[0];
     let object = commands[1];
     if (room[action] !== undefined) {
@@ -70,21 +87,3 @@ async function start() {
   }
   process.exit();
 }
-
-/*class Room {
-  constructor() {
-    console.log("Making the room");
-  }
-
-  item(sevenDays) {
-    if (Room === foyer) {
-      console.log("A copy of Seven Days lies in a corner.");
-      return this.item;
-  }
-}
-  inventory(item) {
-    console.log("You are carrying " + item());
-  }
-
-
-//let room = new Room();*/
